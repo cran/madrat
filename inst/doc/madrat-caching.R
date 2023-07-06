78e7386e11a53b33d0b7eebc90b0846ec48a7336
Jan Philipp Dietrich <dietrich@pik-potsdam.de>
2023-07-06 20:50:02 +0000

    version 3.3.5

--- inst/doc/madrat-caching.R
+++ inst/doc/madrat-caching.R
@@ -6,57 +6,61 @@ library(madrat, quietly = TRUE)
 getConfig("cachefolder", verbose = FALSE)
 
 ## ---- echo = TRUE, eval=TRUE--------------------------------------------------
-getDependencies("calcTauTotal")
+getDependencies("calcTauTotal", packages = "madrat")
 
 ## ---- echo = TRUE, eval=TRUE--------------------------------------------------
-setConfig(verbosity=3)
-fp <- madrat:::fingerprint("calcTauTotal", details = TRUE)
+setConfig(verbosity = 3)
+fp <- madrat:::fingerprint("calcTauTotal", details = TRUE, packages = "madrat")
 
 ## ---- echo = TRUE, eval=FALSE-------------------------------------------------
-#    "!# @monitor madrat:::sysdata magclass:::ncells"
+#  "!# @monitor madrat:::sysdata magclass:::ncells"
 #    "!# @ignore  madrat:::toolAggregate"
 
 ## ---- echo = TRUE, eval=TRUE--------------------------------------------------
-  setConfig(globalenv = TRUE)
+setConfig(globalenv = TRUE)
   readData <- function() return(1)
   readData2 <- function() return(2)
   calcExample <- function() {
     a <- readSource("Data")
+    return(a)
   }
   calcExample2 <- function() {
     a <- readSource("Data")
-    if(FALSE) b <- readSource("Data2")
+    if (FALSE) a <- readSource("Data2")
+    return(a)
   }
 
 ## ---- echo = TRUE, eval=TRUE--------------------------------------------------
-  fp <- madrat:::fingerprint("calcExample", details = TRUE)
-  fp2 <- madrat:::fingerprint("calcExample2", details = TRUE)
+fp <- madrat:::fingerprint("calcExample", details = TRUE, packages = "madrat")
+  fp2 <- madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat")
 
 ## ---- echo = TRUE, eval=TRUE--------------------------------------------------
-  readData <- function() return(99)
-  fp <- madrat:::fingerprint("calcExample", details = TRUE)
-  fp2 <- madrat:::fingerprint("calcExample2", details = TRUE)
+readData <- function() return(99)
+fp <- madrat:::fingerprint("calcExample", details = TRUE, packages = "madrat")
+fp2 <- madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat")
 
 ## ---- echo = TRUE, eval=TRUE--------------------------------------------------
-  readData2 <- function() {
+readData2 <- function() {
     "!# @monitor madrat:::toolAggregate"
     return(99)
-  } 
-  fp <- madrat:::fingerprint("calcExample", details = TRUE)
-  fp2 <- madrat:::fingerprint("calcExample2", details = TRUE)
+  }
+fp <- madrat:::fingerprint("calcExample", details = TRUE, packages = "madrat")
+fp2 <- madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat")
 
 ## ---- echo = TRUE, eval=TRUE--------------------------------------------------
 
-  calcExample2 <- function() {
+calcExample2 <- function() {
     "!# @ignore readData2"
     a <- readSource("Data")
-    if(FALSE) b <- readSource("Data2")
+    if (FALSE) a <- readSource("Data2")
+    return(a)
   }
 
   calcExample3 <- function() {
     a <- calcOutput("Example2")
+    return(a)
   }
 
-  fp2 <- madrat:::fingerprint("calcExample2", details = TRUE)
-  fp3 <- madrat:::fingerprint("calcExample3", details = TRUE)
+fp2 <- madrat:::fingerprint("calcExample2", details = TRUE, packages = "madrat")
+fp3 <- madrat:::fingerprint("calcExample3", details = TRUE, packages = "madrat")
 
